a3f4ccdf297aa4a56cbfd7ecae70401e72fa20ed
bactran1 <56278118+bactran1@users.noreply.github.com>
2020-03-10 05:47:45 -0700

    fix email layout

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv/config');
 const readline = require('readline');
 const Joi = require('joi');
 const ejs = require('ejs');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
